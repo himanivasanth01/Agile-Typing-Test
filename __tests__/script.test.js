eb33b24c6672e26fcd54c1ab125d518c9efc4705
himanivasanth01 <155656609+himanivasanth01@users.noreply.github.com>
2025-05-15 07:15:26 +0530

    Create script.test.js

---
+++ __tests__/script.test.js
@@ -0,0 +1,13 @@
+const { calculateAccuracy } = require('../script');
+
+test('calculates 80% accuracy for 8 out of 10', () => {
+    expect(calculateAccuracy(8, 10)).toBe(80);
+});
+
+test('returns 0 if wordsSubmitted is 0', () => {
+    expect(calculateAccuracy(5, 0)).toBe(0);
+});
+
+test('calculates 100% accuracy for 10 out of 10', () => {
+    expect(calculateAccuracy(10, 10)).toBe(100);
+});
